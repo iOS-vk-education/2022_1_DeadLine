61ccc11ddcabade1182f4c39d1f1bc1a5a4c8139
kravtandr <kravtandr@gmail.com>
2022-04-19 21:20:57 +0300

    Add Tasks Screen

---
+++ DeadLine/DeadLine/DemoTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  DemoTableViewCell.swift
+//  DeadLine
+//
+//  Created by Андрей Кравцов on 19.04.2022.
+//
+
+import UIKit
+
+class DemoTableViewCell: UITableViewCell {
+    
+    @IBOutlet var myLablel: UILabel!
+    @IBOutlet var myOption: UIButton!
+    
+    
+    @IBOutlet weak var cell: UIView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        cell.layer.cornerRadius = 15
+        
+        cell.layer.shadowRadius = 5
+        cell.layer.shadowOpacity = 0.25
+        cell.layer.shadowOffset = CGSize(width: 0, height: 3)
+    
+    }
+    
+    func setCellHeader(_ title:String){
+        self.myLablel.text = title
+    }
+    
+    @IBAction func radioSelected(_ sender: UIButton){
+        self.myOption.isSelected = !self.myOption.isSelected
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
---
+++ DeadLine/DeadLine/TasksViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FindViewController.swift
+//  DeadLine
+//
+//  Created by Roman Nizovtsev on 18.04.2022.
+//
+
+import UIKit
+
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
+    
+    @IBOutlet var tableView: UITableView!
+    let myData = ["first","second","third", "four", "five","first","second","third", "four", "five"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "DemoTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "DemoTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsSelection = false
+    }
+    
+
+    //Table FN
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell",
+                                                 for: indexPath) as! DemoTableViewCell
+        cell.myLablel.text =  myData[indexPath.row]
+        
+        return cell
+    }
+
+}

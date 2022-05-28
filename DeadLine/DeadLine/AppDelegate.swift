9818ecae77b1a29d211c3acfc65664a49744680f
Roman Nizovtsev <romanizovtsev@mail.ru>
2022-05-28 20:42:53 +0300

    hop-hey-lalaley

--- DeadLine/DeadLine/AppDelegate.swift
+++ DeadLine/DeadLine/AppDelegate.swift
@@ -36,6 +36,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    func refresh(application: UIApplication!) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        ViewController().viewDidLoad()
+    }
 
 
 }
